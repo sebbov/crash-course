79e4d4e0f8dac869a68f9af14c36045491ae80a6
Sebastien Boving <github@sebastienboving.com>
2025-04-24 10:04:40 -0700

    Add a welcome model with some help / info.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
 import "./App.css";
 import Chart from "./Chart";
 import crashData from "./crash_data.json";
+import { useState } from "react";
+import "./Modal.css"; // for custom styling
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
+
   return (
     <>
-      <h2>Current stock market crash against major ones</h2>
-      <Chart data={crashData} />
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <h2>Welcome to Crash Course</h2>
+            <br />
+            <p>Hover over chart lines to show their label.</p>
+            <p>Zoom in the chart using your mouse wheel.</p>
+            <br />
+            <p>
+              For background see this{" "}
+              <a href="https://www.reddit.com/r/dataisbeautiful/comments/1k2i027/current_stock_market_crash_against_major_ones_oc/">
+                Reddit thread
+              </a>
+              .
+            </p>
+            <br />
+            <p>
+              Original concept and Python implementation by @resende451 on
+              Twitter (
+              <a href="https://github.com/resendedaniel/drawdown">Source</a>).
+              This site enhances the original by adding interactivity, daily
+              updates, and additional features.
+            </p>
+            <button onClick={() => setShowModal(false)}>OK</button>
+          </div>
+        </div>
+      )}
+
+      <div className={showModal ? "blurred" : ""}>
+        <h2>Current stock market crash against major ones</h2>
+        <Chart data={crashData} />
+        <footer>
+          <a onClick={() => setShowModal(true)} style={{ cursor: "pointer" }}>
+            Help
+          </a>
+          {" | "}
+          <a href="/privacy">Privacy</a>
+          {" | "}
+          <a href="https://github.com/sebbov/crash-course">Source</a>
+        </footer>
+      </div>
     </>
   );
 }
